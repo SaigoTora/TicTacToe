8d1c09cf28a97d91d9dd18d70e2d071306fea73c
Bohdan Kalnytskyi <bogdan201375@gmail.com>
2024-11-28 15:35:39 +0200

    Added the ability to cancel a move in a network game.

--- TicTacToe/Models/GameClientServer/Game/GameInfo.cs
+++ TicTacToe/Models/GameClientServer/Game/GameInfo.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
 using TicTacToe.Models.GameInfo.Settings;
 using TicTacToeLibrary;
@@ -13,16 +14,19 @@ namespace TicTacToe.Models.GameClientServer.Game
 		internal Field Field { get; private set; }
 		[JsonProperty]
 		internal CellType WhoseMove { get; private set; } = CellType.Cross;
+		private readonly Stack<Cell> _sequenceCells;
 
 		private GameInfo() { }
 		internal GameInfo(FieldSize fieldSize)
-			=> Field = FieldParser.Parse(fieldSize);
+		{
+			Field = FieldParser.Parse(fieldSize);
+			_sequenceCells = new Stack<Cell>(Field.GetAllCells().Length);
+		}
 
-		internal void ClearField()
-			=> Field = new Field(Field.GetFieldSize(), Field.WinningCellsCount);
 		internal void Move(Cell cell, CellType cellType)
 		{
 			Field.FillCell(cell, cellType);
+			_sequenceCells.Push(cell);
 			ChangeWhoseMove(cellType);
 		}
 		private void ChangeWhoseMove(CellType cellType)
@@ -32,5 +36,13 @@ namespace TicTacToe.Models.GameClientServer.Game
 			else if (cellType == CellType.Zero)
 				WhoseMove = CellType.Cross;
 		}
+
+		internal void CancelLastMove()
+		{
+			Cell cell = _sequenceCells.Pop();
+			Field.FillCell(cell, CellType.None);
+		}
+		internal void ClearField()
+			=> Field = new Field(Field.GetFieldSize(), Field.WinningCellsCount);
 	}
 }
